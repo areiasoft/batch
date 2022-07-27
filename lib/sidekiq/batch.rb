86b804578f484f90ad2f0e59a7c627c78e38ac99
Timothée Leicht <timothee@adloop.co>
2022-07-27 14:29:09 +0200

    Fixed deprecations

--- lib/sidekiq/batch.rb
+++ lib/sidekiq/batch.rb
@@ -42,12 +42,12 @@ module Sidekiq
       return unless %w(success complete).include?(event.to_s)
       callback_key = "#{@bidkey}-callbacks-#{event}"
       Sidekiq.redis do |r|
-        r.multi do
-          r.sadd(callback_key, JSON.unparse({
+        r.multi do |pipeline|
+          pipeline.sadd(callback_key, JSON.unparse({
             callback: callback,
             opts: options
           }))
-          r.expire(callback_key, BID_EXPIRE_TTL)
+          pipeline.expire(callback_key, BID_EXPIRE_TTL)
         end
       end
     end
@@ -62,10 +62,10 @@ module Sidekiq
           parent_bid = Thread.current[:batch].bid if Thread.current[:batch]
 
           Sidekiq.redis do |r|
-            r.multi do
-              r.hset(@bidkey, "created_at", @created_at)
-              r.hset(@bidkey, "parent_bid", parent_bid.to_s) if parent_bid
-              r.expire(@bidkey, BID_EXPIRE_TTL)
+            r.multi do |pipeline|
+              pipeline.hset(@bidkey, "created_at", @created_at)
+              pipeline.hset(@bidkey, "parent_bid", parent_bid.to_s) if parent_bid
+              pipeline.expire(@bidkey, BID_EXPIRE_TTL)
             end
           end
 
@@ -85,19 +85,19 @@ module Sidekiq
         return [] if @ready_to_queue.size == 0
 
         Sidekiq.redis do |r|
-          r.multi do
+          r.multi do |pipeline|
             if parent_bid
-              r.hincrby("BID-#{parent_bid}", "children", 1)
-              r.hincrby("BID-#{parent_bid}", "total", @ready_to_queue.size)
-              r.expire("BID-#{parent_bid}", BID_EXPIRE_TTL)
+              pipeline.hincrby("BID-#{parent_bid}", "children", 1)
+              pipeline.hincrby("BID-#{parent_bid}", "total", @ready_to_queue.size)
+              pipeline.expire("BID-#{parent_bid}", BID_EXPIRE_TTL)
             end
 
-            r.hincrby(@bidkey, "pending", @ready_to_queue.size)
-            r.hincrby(@bidkey, "total", @ready_to_queue.size)
-            r.expire(@bidkey, BID_EXPIRE_TTL)
+            pipeline.hincrby(@bidkey, "pending", @ready_to_queue.size)
+            pipeline.hincrby(@bidkey, "total", @ready_to_queue.size)
+            pipeline.expire(@bidkey, BID_EXPIRE_TTL)
 
-            r.sadd(@bidkey + "-jids", @ready_to_queue)
-            r.expire(@bidkey + "-jids", BID_EXPIRE_TTL)
+            pipeline.sadd(@bidkey + "-jids", @ready_to_queue)
+            pipeline.expire(@bidkey + "-jids", BID_EXPIRE_TTL)
           end
         end
 
@@ -138,9 +138,9 @@ module Sidekiq
 
     def persist_bid_attr(attribute, value)
       Sidekiq.redis do |r|
-        r.multi do
-          r.hset(@bidkey, attribute, value)
-          r.expire(@bidkey, BID_EXPIRE_TTL)
+        r.multi do |pipeline|
+          pipeline.hset(@bidkey, attribute, value)
+          pipeline.expire(@bidkey, BID_EXPIRE_TTL)
         end
       end
     end
@@ -148,26 +148,26 @@ module Sidekiq
     class << self
       def process_failed_job(bid, jid)
         _, pending, failed, children, complete, parent_bid = Sidekiq.redis do |r|
-          r.multi do
-            r.sadd("BID-#{bid}-failed", jid)
+          r.multi do |pipeline|
+            pipeline.sadd("BID-#{bid}-failed", jid)
 
-            r.hincrby("BID-#{bid}", "pending", 0)
-            r.scard("BID-#{bid}-failed")
-            r.hincrby("BID-#{bid}", "children", 0)
-            r.scard("BID-#{bid}-complete")
-            r.hget("BID-#{bid}", "parent_bid")
+            pipeline.hincrby("BID-#{bid}", "pending", 0)
+            pipeline.scard("BID-#{bid}-failed")
+            pipeline.hincrby("BID-#{bid}", "children", 0)
+            pipeline.scard("BID-#{bid}-complete")
+            pipeline.hget("BID-#{bid}", "parent_bid")
 
-            r.expire("BID-#{bid}-failed", BID_EXPIRE_TTL)
+            pipeline.expire("BID-#{bid}-failed", BID_EXPIRE_TTL)
           end
         end
 
         # if the batch failed, and has a parent, update the parent to show one pending and failed job
         if parent_bid
           Sidekiq.redis do |r|
-            r.multi do
-              r.hincrby("BID-#{parent_bid}", "pending", 1)
-              r.sadd("BID-#{parent_bid}-failed", jid)
-              r.expire("BID-#{parent_bid}-failed", BID_EXPIRE_TTL)
+            r.multi do |pipeline|
+              pipeline.hincrby("BID-#{parent_bid}", "pending", 1)
+              pipeline.sadd("BID-#{parent_bid}-failed", jid)
+              pipeline.expire("BID-#{parent_bid}-failed", BID_EXPIRE_TTL)
             end
           end
         end
